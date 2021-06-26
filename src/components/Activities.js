b2a0c5dcb493ef19e506bd9341de252271b5e747
Francis Lesui <37851797+tflesui@users.noreply.github.com>
2021-06-25 22:09:01 -0700

    Finished rendering cards to Activities page

--- src/components/Activities.js
+++ src/components/Activities.js
@@ -1,10 +1,59 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import ActivitiesCard from './ActivitiesCard';
+import axios from 'axios';
+import { Container, Col, Row, Spinner } from 'reactstrap';
+
 
 const Activities = () => {
+  const [isLoading, setLoading] = useState(true);
+  const [activities, setActivities] = useState();
+
+
+  const getAllActivities = async () => {
+    try {
+      const  { data }  = await axios.get(
+        `https://gentle-woodland-79828.herokuapp.com/api/activities`
+      );
+        
+      // console.log(data)
+      if(data){
+        setActivities(data);
+      }
+      
+      return data || [];
+    } catch (error) {
+      throw error;
+    } finally {
+      setLoading(false);
+    }
+  }
+
+  useEffect(() => {
+    getAllActivities();
+  },[])
+
+  if (isLoading) {
+    return <div className="App"><Spinner type="grow" color="primary" /></div>;
+  }
+
+  const activityCards = activities.map((activity, idx) => {
+    return (
+      <Col sm='4'>
+        <ActivitiesCard key={idx} activity={activity} />
+      </Col>
+    )
+  })
+
   return (
-    <div id="activities">
+    <Container fluid>
+      <div id="activities" className="my-3 mb-3">
         <h1>Activities Page</h1>
-    </div>
+        <Row>
+          {activityCards}
+        </Row>
+      </div>
+    </Container>
+
   );
 }
 
---
+++ src/components/ActivitiesCard.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import {
+  Card, CardText, CardBody,
+  CardTitle, CardSubtitle, Button
+} from 'reactstrap';
+
+const ActivitiesCard = (props) => {
+  // const { id, name, description } = props;
+
+  console.log(props);
+
+  return (
+    <div>
+      <Card className="card">
+        <CardBody>
+          <CardTitle tag="h5" className="text-muted">{`Activity ${props.activity.id}`}</CardTitle>
+          <CardSubtitle tag="h6" className="mb-2 text-muted">{props.activity.name}</CardSubtitle>
+          <CardText className="text-muted">{props.activity.description}</CardText>
+          <Button className="btn-primary">Add to Routine</Button>
+        </CardBody>
+      </Card>
+    </div>
+  );
+};
+
+export default ActivitiesCard; 
